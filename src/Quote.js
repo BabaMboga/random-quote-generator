64cc00c407695d8cb0771a356944ed0f376c8db5
Ayim William <odhisayim@gmail.com>
2024-11-21 15:20:01 +0300

    changed content to quote

--- src/Quote.js
+++ src/Quote.js
@@ -3,37 +3,36 @@ import { useDispatch, useSelector } from "react-redux";
 import { fetchRandomQuote } from "./slices/quoteSlice";
 
 const Quote = () => {
-  const dispatch = useDispatch();
-  const quote = useSelector((state) => state.quote.quote); // Correct access to the quote in state
-  const status = useSelector((state) => state.quote.status);
+    const dispatch = useDispatch();
+    const quote = useSelector((state) => state.quote)
+    const status = useSelector((state) => state.status);
+    useEffect(() => {
+        dispatch(fetchRandomQuote());
+    }, [dispatch]);
+    const handleFetchQuote = () => {
+        dispatch(fetchRandomQuote());
+    };
 
-  useEffect(() => {
-    dispatch(fetchRandomQuote());
-  }, [dispatch]);
-
-  const handleFetchQuote = () => {
-    dispatch(fetchRandomQuote());
-  };
-
-  return (
-    <div className="card col-lg-6 col-md-6 col-10 d-flex justify-content-center">
-      <div className="heading">
-        {status === "loading" ? (
-          <div className="spinner-border text-secondary" role="status">
-            <span className="sr-only">Loading...</span>
-          </div>
-        ) : null}
-        {quote && status !== "loading" ? <div>{quote}</div> : null} {/* Display quote directly */}
-      </div>
-      <button
-        type="button"
-        className="btn btn-primary"
-        onClick={handleFetchQuote}
-      >
-        <span>Generate Quote</span>
-      </button>
-    </div>
-  );
+    return (
+        <div className="card col-lg-6 col-md-6 col-10 d-flex justify-content-center">
+            <div className="heading">
+                {status === "loading" ? (
+                    <div className="spinner-border text-secondary" role="status">
+                        <span className="sr-only">Loading...</span>
+                    </div>
+                ):null}
+                {quote && status !== "loading" ? <div>{quote.quote}</div> : null}
+            </div>
+            <button
+                type="button"
+                className="btn btn-primary"
+                onClick={() => handleFetchQuote()}
+            >
+                <span>Generate Quote</span>
+            </button>
+        </div>
+    );
 };
+
 
 export default Quote;
